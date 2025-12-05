46f5a3c147f7a2198442389142b01ab74d276946
Tammam Bettayeb <tambett10@gmail.com>
2025-12-05 08:53:35 +0100

    Added user functionnality (without the challenge_completion feature waiting for the challenge functinnality)

--- include/user.h
+++ include/user.h
@@ -4,24 +4,35 @@
 //Constants
 #define MAX_USERS 100
 #define MAX_USERNAME 30
+#define CHALLENGE_COUNT 4
 
+#include <stdbool.h>
+
+//User structure
 typedef struct {
     char username[MAX_USERNAME];
     int total_points;
     int number_of_challenges_completed;
+    bool challenges[CHALLENGE_COUNT];
+    int attempts[CHALLENGE_COUNT];
     bool is_logged_in;
 } User;
 
+//User functions
 
+//User object management
 User* user_create(const char* username);
 User* user_login(const char* username);
-void user_logout(User* user);
+User* user_logout(User* user);
+User* user_save_to_file(const User* user);
+User* user_load_from_file(const char* username);
+
+//User data retrieval and modification
 int user_points(const User* user);
 int user_number_of_challenges(const User* user);
-void user_add_points(User* user, const int points);
-void user_add_challenge_completed(User* user);
-void user_save_to_file(const User* user);
-User* user_load_from_file(const char* username);
+User* user_add_points(User* user, const int points);
+User* user_add_challenge_completed(User* user, const int n);
+
 
 
 #endif
